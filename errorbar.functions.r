008af72d1dec2e01fd193b1ff002473f9e258f3a
fitz90 <alex.filazzola@outlook.com>
2016-08-03 13:59:35 -0400

    added further analyses

---
+++ errorbar.functions.r
@@ -0,0 +1,8 @@
+#functions
+se <- function(x) sqrt(var(x)/length(x))
+
+error.bar <- function(x, y, upper, lower=upper, length=0,...){
+  if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
+    stop("vectors must be same length")
+  arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, ...)
+}
